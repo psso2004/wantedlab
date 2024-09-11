ca104b3347310f146767c06e151f3ce2f9a67b8b
psso2004 <pung-gigwon@pung-gigwon-ui-MacBookAir.local>
2024-09-12 02:02:22 +0900

    feat: comment output dto 수정(children 수정)

--- src/board/comment/dtos/outputs/comment.output.dto.ts
+++ src/board/comment/dtos/outputs/comment.output.dto.ts
@@ -3,12 +3,14 @@ import { CommentEntity } from "../../entities/comment.entity";
 export class CommentOutputDto {
   content: string;
   userName: string;
-  children: CommentOutputDto;
+  children: CommentOutputDto[];
   createdAt: Date;
 
   constructor(source: CommentEntity) {
     this.content = source.content;
     this.userName = source.userName;
+    this.children =
+      source.children?.map((child) => new CommentOutputDto(child)) || [];
     this.createdAt = source.createdAt;
   }
 }
