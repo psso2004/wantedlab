e810be80542165de79201635e864f4d70dfba9b2
psso2004 <pung-gigwon@pung-gigwon-ui-MacBookAir.local>
2024-09-12 19:13:42 +0900

    feat: get post query dto 추가

--- src/board/modules/post/post.controller.ts
+++ src/board/modules/post/post.controller.ts
@@ -17,16 +17,20 @@ import { DataSource } from "typeorm";
 import { PostEntity } from "./entities/post.entity";
 import { PaginatedOutput } from "../../dtos/outputs/paginated.output.dto";
 import { IPaginated } from "../../dtos/interfaces/paginated.interface";
+import { GetPostQueryDto } from "./dtos/queries/get-post.query.dto";
 
 @Controller("post")
 export class PostController {
   constructor(private readonly dataSource: DataSource) {}
 
   @Get()
-  async getPosts(@Query() query: any): Promise<IPaginated<PostOutputDto>> {
+  async getPosts(
+    @Query() query: GetPostQueryDto
+  ): Promise<IPaginated<PostOutputDto>> {
+    const { page, limit } = query;
     const repo = this.dataSource.getRepository(PostEntity);
     const [posts, total] = await Promise.all([repo.find(), repo.count()]);
-    return PaginatedOutput(PostOutputDto, posts, total, 1, 20);
+    return PaginatedOutput(PostOutputDto, posts, total, page, limit);
   }
 
   @Post()
