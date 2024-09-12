cb67e237eaa53ce3a03a2de9b520913750643ce6
psso2004 <pung-gigwon@pung-gigwon-ui-MacBookAir.local>
2024-09-13 06:06:37 +0900

    feat: 키워드 서비스 추가

--- src/keyword/keyword.module.ts
+++ src/keyword/keyword.module.ts
@@ -2,9 +2,10 @@ import { Module } from "@nestjs/common";
 import { TypeOrmModule } from "@nestjs/typeorm";
 import { KeywordEntity } from "./entities/keyword.entity";
 import { KeywordConsumer } from "./keyword.consumer";
+import { KeywordService } from './keyword.service';
 
 @Module({
   imports: [TypeOrmModule.forFeature([KeywordEntity])],
-  providers: [KeywordConsumer],
+  providers: [KeywordConsumer, KeywordService],
 })
 export class KeywordModule {}
