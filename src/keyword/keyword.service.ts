1571eee9d5d6bea1a2fb7344cd3a0d84a7ad2099
psso2004 <pung-gigwon@pung-gigwon-ui-MacBookAir.local>
2024-09-13 06:52:17 +0900

    feat: 키워드 서비스  조회 기능구현

--- src/keyword/keyword.service.ts
+++ src/keyword/keyword.service.ts
@@ -1,4 +1,16 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable } from "@nestjs/common";
+import { DataSource, EntityManager, FindOptionsWhere } from "typeorm";
+import { KeywordEntity } from "./entities/keyword.entity";
 
 @Injectable()
-export class KeywordService {}
+export class KeywordService {
+  constructor(private readonly dataSource: DataSource) {}
+
+  getKeyword(
+    where: FindOptionsWhere<KeywordEntity>,
+    entityManager?: EntityManager
+  ): Promise<KeywordEntity | null> {
+    const em = entityManager ?? this.dataSource.createEntityManager();
+    return em.findOneBy(KeywordEntity, where);
+  }
+}
