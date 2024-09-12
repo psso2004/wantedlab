de57377ab8bd741a8b722ceaf8ddaf366693a92a
psso2004 <pung-gigwon@pung-gigwon-ui-MacBookAir.local>
2024-09-12 16:45:51 +0900

    feat: typeorm 설정 변경

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,10 +3,11 @@ import { AppController } from "./app.controller";
 import { AppService } from "./app.service";
 import { TypeOrmModule } from "@nestjs/typeorm";
 import { ConfigModule, ConfigService } from "@nestjs/config";
-import { AppDataSource } from "./data-source";
 import { BoardModule } from "./board/board.module";
 import { KeywordModule } from "./keyword/keyword.module";
 import { JoiPipeModule } from "nestjs-joi";
+import { SnakeNamingStrategy } from "typeorm-naming-strategies";
+import { DataSource } from "typeorm";
 
 @Module({
   imports: [
@@ -15,11 +16,23 @@ import { JoiPipeModule } from "nestjs-joi";
     }),
     TypeOrmModule.forRootAsync({
       imports: [ConfigModule],
-      useFactory: async (configService: ConfigService) => {
-        const dataSource = AppDataSource(configService);
-        return dataSource.options;
-      },
       inject: [ConfigService],
+      useFactory: (configService: ConfigService) => ({
+        type: configService.get<"mysql" | "mariadb">("DB_TYPE", "mysql"),
+        host: configService.get<string>("DB_HOST"),
+        port: configService.get<number>("DB_PORT"),
+        username: configService.get<string>("DB_USERNAME"),
+        password: configService.get<string>("DB_PASSWORD"),
+        database: configService.get<string>("DB_DATABASE"),
+        entities: [__dirname + "/**/*.entity{.ts,.js}"],
+        synchronize: false,
+        logging: ["error", "query"],
+        namingStrategy: new SnakeNamingStrategy(),
+      }),
+      dataSourceFactory: async (options) => {
+        const dataSource = await new DataSource(options).initialize();
+        return dataSource;
+      },
     }),
     JoiPipeModule.forRoot({
       pipeOpts: {
