e29348ce84f76923790144d6b18cae6fe8d8500d
psso2004 <pung-gigwon@pung-gigwon-ui-MacBookAir.local>
2024-09-12 21:25:15 +0900

    feat: get comment query dto 추가

---
+++ src/board/modules/comment/dtos/queries/get-comment.query.dto.ts
@@ -0,0 +1,13 @@
+import { JoiSchema, JoiSchemaOptions } from "nestjs-joi";
+import * as Joi from "joi";
+
+@JoiSchemaOptions({
+  allowUnknown: false,
+})
+export class GetCommentQueryDto {
+  @JoiSchema(Joi.number().integer().positive().default(1))
+  page?: number;
+
+  @JoiSchema(Joi.number().integer().positive().default(10))
+  limit?: number;
+}
