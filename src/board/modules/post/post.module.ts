f06446ffe00b7a547f12b5f1f101d9ff10177654
psso2004 <pung-gigwon@pung-gigwon-ui-MacBookAir.local>
2024-09-12 22:53:03 +0900

    feat: post module export 설정

--- src/board/modules/post/post.module.ts
+++ src/board/modules/post/post.module.ts
@@ -3,11 +3,12 @@ import { TypeOrmModule } from "@nestjs/typeorm";
 import { PostEntity } from "./entities/post.entity";
 import { PostCommentEntity } from "./entities/post-comment.entity";
 import { PostController } from "./post.controller";
-import { PostService } from './post.service';
+import { PostService } from "./post.service";
 
 @Module({
   imports: [TypeOrmModule.forFeature([PostEntity, PostCommentEntity])],
   controllers: [PostController],
   providers: [PostService],
+  exports: [PostService],
 })
 export class PostModule {}
