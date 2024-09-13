38f0131280a4a34ddd54d804bf25c03f077051fe
psso2004 <pung-gigwon@pung-gigwon-ui-MacBookAir.local>
2024-09-14 01:12:57 +0900

    feat: comment output dto 수정 (id 추가)

--- src/board/modules/comment/dtos/outputs/comment.output.dto.ts
+++ src/board/modules/comment/dtos/outputs/comment.output.dto.ts
@@ -1,17 +1,20 @@
 import { CommentEntity } from "../../entities/comment.entity";
 
 export class CommentOutputDto {
+  id: number;
   content: string;
   userName: string;
   children: CommentOutputDto[];
   createdAt: Date;
 
   constructor(
+    id: number,
     content: string,
     userName: string,
     children: CommentEntity[] = [],
     createdAt: Date
   ) {
+    this.id = id;
     this.content = content;
     this.userName = userName;
     this.children = children.map((child) => CommentOutputDto.fromEntity(child));
@@ -20,6 +23,7 @@ export class CommentOutputDto {
 
   static fromEntity(source: CommentEntity) {
     return new CommentOutputDto(
+      source.id,
       source.content,
       source.userName,
       source.children,
